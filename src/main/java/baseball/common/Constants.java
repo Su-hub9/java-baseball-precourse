5a5f20301394c3d238d76bd882b1745776aa03f6
Su-hub9 <su9581@gmail.com>
2022-04-18 13:04:38 +0900

    feat(common): create common constants class

---
+++ src/main/java/baseball/common/Constants.java
@@ -0,0 +1,21 @@
+package baseball.common;
+
+/**
+ * Common Constants
+ *
+ * @author suji
+ * @date 2022-04-15
+ **/
+public class Constants {
+
+    public static int NUMBER_SIZE = 3;
+    public static int RANGE_START = 1;
+    public static int RANGE_END = 9;
+
+    // Enum 클래스로 처리하면 어떨지 생각 해봐야 함
+    public static String STATE_ONGOING = "ONGOING";
+    public static String STATE_FINISH = "FINISH";
+    public static String STATE_EXIT = "EXIT";
+
+    public static String GAME_RESTART = "1";
+}
---
+++ src/main/java/baseball/common/Message.java
@@ -0,0 +1,19 @@
+package baseball.common;
+
+/**
+ * Message
+ *
+ * @author suji
+ * @date 2022-04-15
+ **/
+public class Message {
+
+    public static String REQUEST_INPUT_NUMBERS = "숫자를 입력해주세요 : ";
+    public static String FINISH_GAME = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static String REQUEST_RESTART_OR_EXIT = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    public static String EXIT_GAME = "프로그램을 종료합니다.";
+
+    public static String ERROR_INPUT_DUPLICATION = "중복된 숫자가 입력 되었습니다.";
+    public static String ERROR_WRONG_INPUT = "잘못된 값이 입력 되었습니다.";
+    public static String ERROR_CHECK_RESULTS = "숫자 판별 시 오류가 발생 했습니다.";
+}
